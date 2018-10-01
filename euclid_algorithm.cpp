5cd41d8e130baae87c117e044917baa57b16085b
Osvaldo <ocstllns@gmail.com>
2018-09-30 21:42:11 -0500

    Adding the template for homework on Euclidean Algorithm

---
+++ euclid_algorithm.cpp
@@ -0,0 +1,28 @@
+/*
+ This software provides two functions that
+
+ 1) finds the greatest common divisor between two numbers.
+ int gcd(int a, int b) // returns gcd(a,b)
+
+ 2) It computes the inverse element:
+ int inverse(int a, int m) // it returns b such that ab=1(mod m)
+*/
+
+int gcd(int a, int b)
+{
+
+}
+
+int inverse(int a, int m)
+{
+
+}
+
+
+int main()
+{
+
+
+
+return 0;
+}
